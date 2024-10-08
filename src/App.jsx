3c89c5ef4ca02d9a45e1d9497e8f7ac00410b7ac
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-10-08 11:34:10 +0400

    feature/localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const [value, setValue] = useState('');
-  const [inputvale, setinputValue] = useState('');
   const [valuesArray, setValuesArray] = useState([]);
 
-  const handleInputValue = (e) => {
+  useEffect(() => {
+    const localStorageValue = JSON.parse(localStorage.getItem('valueArray'));
+    if (localStorageValue) {
+      setValuesArray(localStorageValue);
+    }
+  }, []);
+
+  const handleValue = (e) => {
     setValue(e.target.value);
   };
 
-  const handValue = (e) => {
+  const handleSetValue = (e) => {
     e.preventDefault();
-    setinputValue(value);
-    setValuesArray((perv) => setValuesArray([...perv, inputvale]));
+
+    setValuesArray((prev) => {
+      const setLocalArry = [...prev, value];
+
+      localStorage.setItem('valueArray', JSON.stringify(setLocalArry));
+
+      return setLocalArry;
+    });
+    setValue('');
   };
 
   return (
     <>
       <h1 className='text-3xl font-bold text-center'>Midterm 2</h1>
-      <form>
+      <form onSubmit={handleSetValue}>
         <input
           type='text'
           value={value}
           placeholder='input'
-          onChange={handleInputValue}
+          onChange={handleValue}
         />
-        <button type='submit' onClick={handValue}>
-          btnv
-        </button>
+        <button type='submit'>btn</button>
       </form>
-      <p>{valuesArray}</p>
+      <ul>
+        {valuesArray.map((value, index) => (
+          <li key={`${value} + ${index}`}>{value}</li>
+        ))}
+      </ul>
     </>
   );
 }
